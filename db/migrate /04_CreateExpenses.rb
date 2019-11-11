bbae6e159cc4e61ae26cb8f7a4d88e48ca8fc7d8
siapankina <siapankina@gmail.com>
2019-11-11 16:12:21 +0000

    wrote the class Expense and created migration for Expanses table

---
+++ db/migrate /04_CreateExpenses.rb
@@ -0,0 +1,11 @@
+class CreateExpenses < ActievRecord::Migration[5.2]
+  def change
+    create_table :expenses do |t|
+      t.string :name
+      t.integer :amount
+      t.integer :user_id
+      t.integer :budget_id
+      t.integer :category_id
+    end
+  end
+end
