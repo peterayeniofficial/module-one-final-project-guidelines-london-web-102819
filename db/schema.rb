63d52e16041da609a4534eaa82af7f03a93f01e1
Peter Ayeni <ayenicology@gmail.com>
2019-11-11 16:42:18 +0000

    Created Budget migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_11_164053) do
+
+  create_table "budgets", force: :cascade do |t|
+    t.string "month"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
